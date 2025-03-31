638e65c4fbf0c7c10f0a7c33da0a564034ab096f
wlsp <wlsp@outlook.de>
2025-03-31 18:44:50 +0200

    - added user interaction via terminal for password validation - added methode that calls all methods - added System.out.println for each method so the user know what to do better while choosing a password

--- src/main/java/wlsp/tech/Main.java
+++ src/main/java/wlsp/tech/Main.java
@@ -1,6 +1,7 @@
 package wlsp.tech;
 
 //import java.util.regex.Matcher;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -11,61 +12,113 @@ public class Main {
     static Pattern MIN_LENGTH_REGEX = Pattern.compile(".{8,}$");
     static Pattern MIN_LENGTH_REGEX_NO_WHITESPACE_REGEX = Pattern.compile("^(\\S{8,})$");
     static Pattern HAS_AT_LEAST_ONE_NUMBER_REGEX = Pattern.compile(".*[0-9].*");
-    static Pattern IS_ONLY_NUMBER_REGEX = Pattern.compile("^[0-9]");
+    static Pattern IS_ONLY_NUMBER_REGEX = Pattern.compile("^[0-9]+$");
     static Pattern HAS_LOWERCASE_AND_UPPERCASE_REGEX = Pattern.compile("(?=.*[a-z])(?=.*[A-Z]).*");
     static Pattern HAS_AT_LEAST_ONE_SPECIAL_CHARACTER_REGEX = Pattern.compile(".*[#!?@$%^&*-].*");
-    static String[] FORBIDDEN_PASSWORDS = {"Password123", "123Password", "Abc123", "123ABc"};
-
-
-   /*@TODO to implement when all methods are ready to use
-    Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a password");
-    String password = sc.nextLine();
-        sc.close();
-    */
+    static String[] FORBIDDEN_PASSWORDS = {"Password123!", "123Password", "Abc123", "123ABc"};
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please go ahead, enter a password...");
+        String password = sc.nextLine();
+        sc.close();
+
+        validatePassword(password);
     }
 
     //Password should have at least 8 Characters
     public static boolean minLength(String password) {
-        return MIN_LENGTH_REGEX.matcher(password).matches();
+        boolean isValid = MIN_LENGTH_REGEX.matcher(password).matches();
+
+        if (!isValid) {
+            System.out.println("Password has not been minimum length!");
+        }
+
+        return isValid;
     }
 
     //Password should not contain whitespace
     public static boolean minLengthWithOutWhitespace(String password) {
-        return MIN_LENGTH_REGEX_NO_WHITESPACE_REGEX.matcher(password).matches();
+
+        boolean isValid = MIN_LENGTH_REGEX_NO_WHITESPACE_REGEX.matcher(password).matches();
+
+        if (!isValid) {
+            System.out.println("Whitespace is not allowed!");
+        }
+
+        return isValid;
     }
 
     //Password should have at least one number
     public static boolean hasNumber(String password) {
-        return HAS_AT_LEAST_ONE_NUMBER_REGEX.matcher(password).matches();
+        boolean isValid = HAS_AT_LEAST_ONE_NUMBER_REGEX.matcher(password).matches();
+        if (!isValid) {
+            System.out.println("At least one number is required!");
+        }
+        return isValid;
     }
 
     //Password should NOT be only NUMBERS
-    public static boolean isOnlyNumbers( String password) {
-        return IS_ONLY_NUMBER_REGEX.matcher(password).matches();
+    public static boolean isOnlyNumbers(String password) {
+        boolean isOnlyNumbers = IS_ONLY_NUMBER_REGEX.matcher(password).matches();
+
+        if (isOnlyNumbers) {
+            System.out.println("❌ Password cannot be only numbers!");
+            return false;
+        }
+
+        return true;
     }
 
     //Password should have at least one upper and one lower case letter
     public static boolean hasLowercaseAndUppercase(String password) {
-        return HAS_LOWERCASE_AND_UPPERCASE_REGEX.matcher(password).matches();
+        boolean isValid = HAS_LOWERCASE_AND_UPPERCASE_REGEX.matcher(password).matches();
+
+        if (!isValid) {
+            System.out.println("At least one uppercase and one lowercase character is required!");
+        }
+
+        return isValid;
     }
 
     //Password should have at least one special character
     public static boolean hasAtLeastOneSpecialCharacter(String password) {
-        return HAS_AT_LEAST_ONE_SPECIAL_CHARACTER_REGEX.matcher(password).matches();
+        boolean isValid = HAS_AT_LEAST_ONE_SPECIAL_CHARACTER_REGEX.matcher(password).matches();
+
+        if (!isValid) {
+            System.out.println("❌ At least one special character is required!");
+        }
+
+        return isValid;
     }
+
 
     //Password should NOT be one of those @FORBIDDEN_PASSWORDS
     public static boolean isNotFrequentlyUse(String password) {
         for (String pwd: FORBIDDEN_PASSWORDS) {
             if (pwd.equals(password)) {
-                return true;
+                System.out.println("Nice try diddy - not that one! That's to easy, try again!");
+                return false;
             }
         }
-        return false;
+        return true;
+    }
+
+    public static void validatePassword(String password) {
+        boolean validated  = minLength(password)
+                && minLengthWithOutWhitespace(password)
+                && hasNumber(password)
+                && isOnlyNumbers(password)
+                && hasLowercaseAndUppercase(password)
+                && hasAtLeastOneSpecialCharacter(password)
+                && isOnlyNumbers(password)
+                && isNotFrequentlyUse(password);
+
+        if (validated) {
+            System.out.println("Password has been validated!");
+        }
+
+
     }
 
 }
