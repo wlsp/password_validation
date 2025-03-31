b173bcf60096c81e167182dc197c84d078ec7b58
wlsp <wlsp@outlook.de>
2025-03-31 13:00:06 +0200

    Initial commit

---
+++ src/test/java/wlsp/tech/MainTest.java
@@ -0,0 +1,10 @@
+package wlsp.tech;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
